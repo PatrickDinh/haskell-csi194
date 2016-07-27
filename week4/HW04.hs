1d06066c89a1ac51eee6618bba9e2f12129293b1
Patrick Dinh <d.conghoang@gmail.com>
2016-07-27 17:15:32 +1000

    Week 4

--- week4/HW04.hs
+++ week4/HW04.hs
@@ -6,22 +6,46 @@ newtype Poly a = P [a]
 -- Exercise 1 -----------------------------------------
 
 x :: Num a => Poly a
-x = undefined
+x = P [0, 1]
 
 -- Exercise 2 ----------------------------------------
 
 instance (Num a, Eq a) => Eq (Poly a) where
-    (==) = undefined
+    (==) (P []) (P []) = True
+    (==) (P [0]) (P []) = True
+    (==) (P []) (P [0]) = True
+    (==) (P (h:t)) (P []) = if h == 0 then (==) (P t) (P []) else False
+    (==) (P []) (P (h:t)) = if h == 0 then (==) (P []) (P t) else False
+    (==) (P (h1:t1)) (P (h2:t2)) = if h1 == h2 then (==) (P t1) (P t2) else False
  
 -- Exercise 3 -----------------------------------------
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
-    show = undefined
-
+    show (P arr) = 
+        let revArr = reverse arr
+        in show' revArr
+        where show' :: (Num a1, Eq a1, Show a1) => [a1] -> String
+              show' [] = ""
+              show' [0] = ""
+              show' [n] = show (n)
+              show' [0, n] = if n /= 0 then show' [n] else ""
+              show' [1, n] = "x + " ++ (if n /= 0 then show' [n] else "")
+              show' [-1, n] = "-x + " ++ (if n /= 0 then show' [n] else "")
+              show' [n1, n2] = show(n1) ++ "x" ++ (if n2 /= 0 then show' [n2] else "")
+              show' (0:t) = show' t
+              show' (1:t) = "x^" ++ show (length t) ++ " + " ++ show' t
+              show' (-1:t) = "-" ++ show' (1:t)
+              show' (h:0:t) = (show h) ++ "x^" ++ show (length t + 1) ++ show' t
+              show' (h:t) = (show h) ++ "x^" ++ show (length t) ++ " + " ++ show' t
 -- Exercise 4 -----------------------------------------
 
 plus :: Num a => Poly a -> Poly a -> Poly a
-plus = undefined
+plus (P a) (P b) = P (mergeArr a b)
+    where mergeArr::(Num a) => [a] -> [a] -> [a]
+          mergeArr [] [] = []
+          mergeArr arr [] = arr
+          mergeArr [] arr = arr
+          mergeArr (h1:t1) (h2:t2) = (h1+h2):(mergeArr t1 t2)
 
 -- Exercise 5 -----------------------------------------
 
